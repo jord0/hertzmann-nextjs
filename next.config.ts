d2d2bf42cbdecf0811beff101c1222ceebc9e941
jord0 <jpresnick@gmail.com>
2026-02-09 12:54:23 -0800

    Switch from export to SSG mode

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
-  },
-  trailingSlash: true,
-  env: {
-    DB_HOST: process.env.DB_HOST,
-    DB_PORT: process.env.DB_PORT,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_NAME: process.env.DB_NAME,
   },
 };
 
